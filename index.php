cb3f405ac4b686c3fa75f9518a0b8e6823d4cb46
shubham kumar <shubham.webdev@theschoolsocial.in>
2023-01-05 15:59:21 +0530

    all- code

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/user.css">
+</head>
+<body>
+    <?php require "navbar.php" ?>
+
+    <section class="banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1 class="title text-light">When Milk Met Icecream Love Invitable</h1>
+                    <p class="desc text-light">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque sunt
+                        corrupti blanditiis consequuntur quasi, facilis neque, praesentium corporis, porro consectetur
+                        tenetur qui odio voluptatibus rerum quas vero? Sapiente, est rem.</p>
+                    <a href="#" class="btn btn-color">Read More</a>
+                    <a href="#" class="btn btn-trans mx-2">Explore More</a>
+                </div>
+                <div class="col-md-6">
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="box-container">
+            <div class="box">
+                <i class="fas fa-tools"></i>
+                <h3 class="title">Use Website</h3>
+                <span class="container">800</span>
+            </div>
+            <div class="box">
+                <i class="	fas fa-book-reader"></i>
+                <h3 class="title">Reading Book</h3>
+                <span class="container">700</span>
+            </div>
+            <div class="box">
+                <i class="fas fa-chalkboard-teacher"></i>
+                <h3 class="title">Teacher Uses</h3>
+                <span class="container">1200</span>
+            </div>
+            <div class="box">
+                <i class="fas fa-book-open"></i>
+                <h3 class="title">Books</h3>
+                <span class="container">1500</span>
+            </div>
+        </div>
+    </section>
+    <!-- <section class=" zIndex-2"> 
+        <div class="border" style=" background-color:#f39c12;">
+            <div class="container">
+                <a href="/#" type="" style=" background-color:#f39c12; border:none;color:blue;"> <i class="fas fa-save"
+                        style="font-size:60px;"></i></a>
+
+                <a href="login.php" class="profile" type="btn"
+                    style=" background-color:#f39c12; border:none; position:absolute; right: 200px; color:blue;"> <i
+                        class="fas fa-user" style="font-size:60px;"></i></a>
+
+            </div>
+        </div>
+    </section> -->
+   <?php require "profile.php" ?>
+    
+
+     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <script src="./Js/custom.js"></script>
+</body>
+
+</html>
+</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<div class="position-fixed" style="background-color:#f39c12;top:90%;width:99%">
+    <!-- <div class="">
+        <a href=""><i class="fas fa-save"  style="font-size:50px;"></i></a>
+    </div> -->
+    <!-- <div> -->
+        <!-- Example single danger button -->
+        <!-- <div class="btn-group">
+            <a href="#" type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown"
+                aria-expanded="false" style="border:none; position:relitive ; left: 1000px;">
+                <i class="fas fa-user" style="font-size:30px; top:50px;"></i>
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+
+            </ul>
+        </div>
+    </div> -->
+
+    <div class="border" style="background-color:#f39c12;">
+            <div class="container">
+                <a href="/#" type="" style="background-color:#f39c12; border:none;color:blue;"> <i class="fa fa-save"
+                        style="font-size:60px;"></i></a>
+
+                <a href="home.php" class="profile" type="btn"
+                    style=" background-color:#f39c12; border:none; position:absolute; right: 200px; color:blue;"> <i
+                        class="fa fa-user" style="font-size:60px;"></i></a>
+
+            </div>
+        </div>
+</div>
+
+
+<!-- <section class="">
+        <div class="border" style=" background-color:#f39c12;">
+            <div class="container">
+                <a href="/#" type="" style=" background-color:#f39c12; border:none;color:blue;"> <i class="fas fa-save"
+                        style="font-size:60px;"></i></a>
+
+                <a href="login.php" class="profile" type="btn"
+                    style=" background-color:#f39c12; border:none; position:absolute; right: 200px; color:blue;"> <i
+                        class="fas fa-user" style="font-size:60px;"></i></a>
+
+            </div>
+        </div>
+    </section> -->
+
+<body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
